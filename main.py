c19c6fba28b38e75785efdc1f7343264bed6fdc5
Caleb <calebkugel1@gmail.com>
2022-02-08 23:32:13 -0600

    moment

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 import cv2 as cv  # OpenCV computer vision library
 import numpy as np  # Scientific computing library
 
-
 # Just use a subset of the classes
 classes = ["cargo_red", "cargo_blue"]
 
@@ -21,14 +20,15 @@ pbt = 'Balls_label_map.pbtxt'
 
 # Read the neural network
 cvNet = cv.dnn.readNetFromTensorflow(pb, pbt)
+cvNet.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+cvNet.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
 while True:
-
     # Read in the frame
     ret_val, img = cam.read()
     rows = img.shape[0]
     cols = img.shape[1]
-    cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
+    cvNet.setInput(cv.dnn.blobFromImage(img))
 
     # Run object detection
     cvOut = cvNet.forward()
@@ -37,7 +37,6 @@ while True:
     for detection in cvOut[0, 0, :, :]:
         score = float(detection[2])
         if score > 0.3:
-
             idx = int(detection[1])  # prediction class index.
 
             # If you want all classes to be labeled instead of just forks, spoons, and knives,
