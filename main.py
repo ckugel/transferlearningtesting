fcd590f5b64a8b0b504583f2ae9636b068ec65f0
Caleb Kugel <calebkugel1@gmail.com>
2022-02-10 20:59:29 -0600

    yolo
    It's horrible please do not use this in comp

--- main.py
+++ main.py
@@ -1,67 +1,120 @@
-# Project: Eating Utensil Detector Using TensorFlow and OpenCV
-# Author: Addison Sears-Collins
-# Date created: August 1, 2021
-# Description: This program detects forks, spoons, and knives
+import torch
+import numpy as np
+import cv2
+from time import time
 
-import cv2 as cv  # OpenCV computer vision library
-import numpy as np  # Scientific computing library
 
-# Just use a subset of the classes
-classes = ["cargo_red", "cargo_blue"]
+class MugDetection:
+    """
+    Class implements Yolo5 model to make inferences on a youtube video using Opencv2.
+    """
 
-# Colors we will use for the object labels
-colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    def __init__(self, capture_index, model_name):
+        """
+        Initializes the class with youtube url and output file.
+        :param url: Has to be as youtube URL,on which prediction is made.
+        :param out_file: A valid output file name.
+        """
+        self.capture_index = capture_index
+        self.model = self.load_model(model_name)
+        self.classes = self.model.names
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("Using Device: ", self.device)
 
-# Open the webcam
-cam = cv.VideoCapture(0)
+    def get_video_capture(self):
+        """
+        Creates a new video streaming object to extract video frame by frame to make prediction on.
+        :return: opencv2 video capture object, with lowest quality frame available for video.
+        """
 
-pb = 'frozen_inference_graph.pb'
-pbt = 'Balls_label_map.pbtxt'
+        return cv2.VideoCapture(self.capture_index)
 
-# Read the neural network
-cvNet = cv.dnn.readNetFromTensorflow(pb, pbt)
-cvNet.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-cvNet.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+    def load_model(self, model_name):
+        """
+        Loads Yolo5 model from pytorch hub.
+        :return: Trained Pytorch model.
+        """
+        if model_name:
+            model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_name, force_reload=True)
+        else:
+            model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        return model
 
-while True:
-    # Read in the frame
-    ret_val, img = cam.read()
-    rows = img.shape[0]
-    cols = img.shape[1]
-    cvNet.setInput(cv.dnn.blobFromImage(img))
+    def score_frame(self, frame):
+        """
+        Takes a single frame as input, and scores the frame using yolo5 model.
+        :param frame: input frame in numpy/list/tuple format.
+        :return: Labels and Coordinates of objects detected by model in the frame.
+        """
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+        return labels, cord
 
-    # Run object detection
-    cvOut = cvNet.forward()
+    def class_to_label(self, x):
+        """
+        For a given label value, return corresponding string label.
+        :param x: numeric label
+        :return: corresponding string label
+        """
+        return self.classes[int(x)]
 
-    # Go through each object detected and label it
-    for detection in cvOut[0, 0, :, :]:
-        score = float(detection[2])
-        if score > 0.3:
-            idx = int(detection[1])  # prediction class index.
+    def plot_boxes(self, results, frame):
+        """
+        Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
+        :param results: contains labels and coordinates predicted by model on the given frame.
+        :param frame: Frame which has been scored.
+        :return: Frame with bounding boxes and labels ploted on it.
+        """
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.3:
+                x1, y1, x2, y2 = int(row[0] * x_shape), int(row[1] * y_shape), int(row[2] * x_shape), int(
+                    row[3] * y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
 
-            # If you want all classes to be labeled instead of just forks, spoons, and knives,
-            # remove this line below (i.e. remove line 65)
-            if classes[idx] == 'fork' or classes[idx] == 'spoon' or classes[idx] == 'knife':
-                left = detection[3] * cols
-                top = detection[4] * rows
-                right = detection[5] * cols
-                bottom = detection[6] * rows
-                cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+        return frame
 
-                # draw the prediction on the frame
-                label = "{}: {:.2f}%".format(classes[idx], score * 100)
-                y = top - 15 if top - 15 > 15 else top + 15
-                cv.putText(img, label, (int(left), int(y)), cv.FONT_HERSHEY_SIMPLEX, 0.5, colors[idx], 2)
+    def __call__(self):
+        """
+        This function is called when class is executed, it runs the loop to read the video frame by frame,
+        and write the output into a new file.
+        :return: void
+        """
+        cap = self.get_video_capture()
+        assert cap.isOpened()
 
-    # Display the frame
-    cv.imshow('my webcam', img)
+        while True:
 
-    # Press ESC to quit
-    if cv.waitKey(1) == 27:
-        break
+            ret, frame = cap.read()
+            assert ret
 
-# Stop filming
-cam.release()
+            frame = cv2.resize(frame, (416, 416))
 
-# Close down OpenCV
-cv.destroyAllWindows()
+            start_time = time()
+            results = self.score_frame(frame)
+            frame = self.plot_boxes(results, frame)
+
+            end_time = time()
+            fps = 1 / np.round(end_time - start_time, 2)
+            # print(f"Frames Per Second : {fps}")
+
+            cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
+
+            cv2.imshow('YOLOv5 Detection', frame)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+        cap.release()
+
+
+# Create a new object and execute.
+detector = MugDetection(capture_index=0, model_name='best.pt')
+detector()
